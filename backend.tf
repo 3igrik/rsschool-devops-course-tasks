2cb079bbff60b1509e085cf88cfbebf4f1249539
Эрлан Токтосунов <3igrik@gmail.com>
2025-06-14 22:35:18 +0500

    Add code after formatting tf files

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "3igrik-rs-tf-state-2025"
-    key    = "terraform.tfstate"            
-    region = "eu-north-1"                    
-    encrypt = true                          
+    bucket         = "3igrik-rs-tf-state-2025"
+    key            = "terraform.tfstate"
+    region         = "eu-north-1"
+    encrypt        = true
     dynamodb_table = "terraform-state-lock"
   }
 }
--- dynamodb.tf
+++ dynamodb.tf
@@ -1,7 +1,7 @@
 resource "aws_dynamodb_table" "terraform_lock" {
-  name           = var.dynamodb_table_name
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
+  name         = var.dynamodb_table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
 
   attribute {
     name = "LockID"
